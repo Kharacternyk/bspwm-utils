723ae9479fab52677e36cc8fb7ae4c7a00b0cd0c
Nazar Vinnichuk <nazar.vinnichuk@tutanota.com>
2020-05-03 19:19:33 +0300

    Remove old executables.

--- bspwm-undo-close.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-NODE="$(bspc query -N -n .hidden | tail -n1)"
-
-[[ -n $NODE ]] && bspc node $NODE --flag hidden=off
--- bspwm-undoable-close.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-bspc node --flag hidden
-
-sleep 10
-
-NODE="$(bspc query -N -n .hidden | tail -n1)"
-
-[[ -n $NODE ]] && bspc node $NODE --close
