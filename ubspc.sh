4404253407f77ad95ac6929de4a16cf7f7d7ec36
Nazar Vinnichuk <nazar.vinnichuk@tutanota.com>
2020-05-03 19:15:44 +0300

    Integrate functionality into one executable.

---
+++ ubspc.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+undoable_close() {
+    bspc node "$@" --flag hidden
+    sleep 10
+
+    NODE="$(bspc query -N -n .hidden | tail -n1)"
+    [[ -n $NODE ]] && bspc node $NODE --close
+}
+
+undo_close() {
+    NODE="$(bspc query -N -n .hidden | tail -n1)"
+    [[ -n $NODE ]] && bspc node $NODE --flag hidden=off
+}
+
+[[ $# == 0 ]] && exec bspc
+
+case $1 in
+    close)
+        shift
+        undoable_close "$@"
+        ;;
+    undo)
+        shift
+        case $1 in
+            close) shift; undo_close "$@";;
+            *) echo "$1: is not an undoable operation"; exit 1
+        esac
+        ;;
+    *)
+        exec bspc $@
+        ;;
+esac
