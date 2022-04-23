d50eb039b4ecf0d91bbc2f031d822278b8fd6861
Nazar Vinnichuk <nazar@vinnich.uk>
2022-04-23 19:48:42 +0300

    Reformat flake.nix.

--- flake.nix
+++ flake.nix
@@ -1,13 +1,12 @@
 {
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { inherit system; }; in
-      {
-        defaultPackage = pkgs.writeShellScriptBin "ubspc" (builtins.readFile ./ubspc.sh);
-        devShell = pkgs.mkShell {
-          packages = [ self.defaultPackage.${system} ];
-        };
-      }
-    );
+  inputs.utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem (system:
+    with import nixpkgs { inherit system; }; {
+      defaultPackage = pkgs.writeShellScriptBin "ubspc" (builtins.readFile ./ubspc.sh);
+      devShell = pkgs.mkShell {
+        packages = [ self.defaultPackage.${system} ];
+      };
+    }
+  );
 }
