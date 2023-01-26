f3e20d6c5c1daf893c9f9731b64e2551b8ce0bd1
Nazar Vinnichuk <nazar@vinnich.uk>
2023-01-26 19:41:17 +0200

    Refactor flake.nix.

--- flake.nix
+++ flake.nix
@@ -1,12 +1,12 @@
 {
-  inputs.utils.url = "github:numtide/flake-utils";
-
-  outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem (system:
-    with import nixpkgs { inherit system; }; {
-      defaultPackage = pkgs.writeShellScriptBin "ubspc" (builtins.readFile ./ubspc.sh);
-      devShell = pkgs.mkShell {
-        packages = [ self.defaultPackage.${system} ];
-      };
+  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
+    let pkgs = import nixpkgs {
+      inherit system;
+    }; in
+    {
+      defaultPackage = pkgs.writeShellScriptBin "ubspc" (
+        builtins.readFile ./ubspc.sh
+      );
     }
   );
 }
